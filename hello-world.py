e24616032b383c006c78f2c89d522017e9e0499b
Rizky Kusuma Nugraha <kusumarizky65@gmail.com>
2025-04-11 13:32:36 +0700

    fitur input data identitas

--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,21 @@
 # Hello World Main Blowing
 print("🔥 Hello World from Main Blowing 🔥\n")
 
-# Fungsi-fungsi akan dipanggil di sini
-# Tapi belum tersedia di branch ini
+def input_identitas():
+    data = []
+    print("== Masukkan 5 Identitas ==")
+    for i in range(5):
+        print(f"Identitas ke-{i+1}")
+        nama = input("Nama: ")
+        umur = input("Umur: ")
+        email = input("Email: ")
+        data.append({
+            "nama": nama,
+            "umur": umur,
+            "email": email
+        })
+        print()
+    return data
 
+# Contoh pemanggilan
 # data = input_identitas()
-# validasi_identitas(data)
