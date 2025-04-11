093234e89dbc1ccd70da5f776d1c152a0bae322d
Rizky Kusuma Nugraha <kusumarizky65@gmail.com>
2025-04-11 13:31:03 +0700

    main fitur

---
+++ hello-world.py
@@ -0,0 +1,8 @@
+# Hello World Main Blowing
+print("🔥 Hello World from Main Blowing 🔥\n")
+
+# Fungsi-fungsi akan dipanggil di sini
+# Tapi belum tersedia di branch ini
+
+# data = input_identitas()
+# validasi_identitas(data)
